212fa81d5577efe7c03ee169fb7d530a29a5254d
Rashmi <poddar.rashmi@gmail.com>
2019-09-04 13:58:12 -0400

    Working on the stretch assignment

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -5,6 +5,12 @@ redBlock.addEventListener('click', () => {
   console.log('The red block is being clicked');
   mainBlock.prepend(redBlock);
 });
+redBlock.addEventListener('mousemove', () => {
+  console.log('The mouse is being pressed down');
+  // setInterval(function() {
+  //   moveRight(redBlock);
+  // }, 2);
+})
 
 
 let blueBlock = document.querySelector('.block--blue');
@@ -30,3 +36,10 @@ grayBlock.addEventListener('click', () => {
   console.log('The gray block is being clicked');
   mainBlock.prepend(grayBlock);
 });
+
+function moveRight(element) { 
+  let left = 10; 
+  position = left + 10;
+  console.log('this function is being called');
+  element.style.left = position + 'px';
+}
